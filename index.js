cb93a43a7fa1472d2a03485b44ddadfe778eb584
Brian Ruddy <briancruddy@gmail.com>
2017-04-26 00:47:51 -0400

    log errors regardless of environment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = (res) => (ex) => {
     let error = process.env.NODE_ENV !== 'production' ? ex : 'An unknown error occurred';
 
+    console.error(typeof ex === 'object' ? JSON.stringify(ex, null, 4) : ex);
     res.status(err.statusCode || 500).json({error});
 };
